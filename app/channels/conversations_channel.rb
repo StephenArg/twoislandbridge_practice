953ac3a9ea71d86118cb1a43803e3d9af6ba0b1e
Stephen Argentina <churchman1993@yahoo.com>
2019-02-25 16:19:05 -0500

    fixed bug where both users unsubscribing would leave conversation still in db

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -1,13 +1,13 @@
 class ConversationsChannel < ApplicationCable::Channel
   def subscribed
-    conversation = Conversation.find(params["conversation_id"])
-    stream_for conversation
+    @conversation = Conversation.find(params["conversation_id"])
+    stream_for @conversation
   end
 
   def unsubscribed
 
     @conversation = Conversation.find(params["conversation_id"])
-    if @conversation.user_id == nil and @conversation.other_user_id == nil
+    if @conversation.user_id == nil or @conversation.other_user_id == nil
       @conversation.destroy
     else
       ConversationsChannel.broadcast_to(@conversation, {send_id: true})
