2d4e3b2ff46700525711ba428b6fadfa34eec136
Stephen Argentina <churchman1993@yahoo.com>
2019-03-06 17:26:35 -0500

    took thing from development and put it in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #https://blog.heroku.com/real_time_rails_implementing_websockets_in_rails_5_with_action_cable
-  Rails.application.config.action_cable.disable_request_forgery_protection = true
-
+  config.action_cable.allowed_request_origins = ['http://localhost:3001', '10.39.105.204:3001']
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
 
 
   #https://blog.heroku.com/real_time_rails_implementing_websockets_in_rails_5_with_action_cable
-  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = ['http://localhost:3001', '10.39.105.204:3001']
 
   #https://blog.heroku.com/real_time_rails_implementing_websockets_in_rails_5_with_action_cable
   config.web_socket_server_url = "wss://https://lit-shore-21918.herokuapp.com/cable"
